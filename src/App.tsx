355994b2077e3a1c0ae6e81abddf4cd29369aed5
Sebastian Rittau <srittau@rittau.biz>
2025-01-28 23:16:37 +0100

    Initialize with vite

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import kokaLogo from '/public/koka.svg'
+import './App.css'
+
+export default function App() {
+  return (
+    <>
+      <div>
+        <img src={kokaLogo} className="logo" alt="koka logo" />
+      </div>
+      <h1>koka</h1>
+    </>
+  )
+}
