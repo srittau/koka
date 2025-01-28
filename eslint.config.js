b07a99bb4694f15dd2d3aee7fb36a2507d5ecfce
Sebastian Rittau <srittau@rittau.biz>
2025-01-28 23:51:44 +0100

    Install eslint-plugin-import

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,9 @@
 import js from "@eslint/js";
-import globals from "globals";
 import prettier from "eslint-config-prettier";
+import importPlugin from "eslint-plugin-import";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
@@ -23,6 +24,26 @@ export default tseslint.config(
       "react-refresh/only-export-components": [
         "warn",
         { allowConstantExport: true },
+      ],
+    },
+  },
+  importPlugin.flatConfigs.recommended,
+  importPlugin.flatConfigs.typescript,
+  {
+    rules: {
+      "import/no-unresolved": "off",
+      "import/first": ["warn"],
+      "import/no-mutable-exports": ["error"],
+      "import/no-self-import": ["error"],
+      "import/no-unused-modules": ["warn"],
+      "import/no-useless-path-segments": ["warn"],
+      "import/order": [
+        "warn",
+        {
+          groups: ["external", "internal", "parent", "sibling"],
+          alphabetize: { order: "asc" },
+          "newlines-between": "always",
+        },
       ],
     },
   },
